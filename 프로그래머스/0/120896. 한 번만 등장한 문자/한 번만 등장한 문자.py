468ab7f13706847004da86f59f82471dece563df
Jiyeon <141120552+smryeonChoi@users.noreply.github.com>
2025-01-25 13:37:50 +0900

    [level 0] Title: 한 번만 등장한 문자, Time: 0.02 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
+++ 프로그래머스/0/120896. 한 번만 등장한 문자/한 번만 등장한 문자.py
@@ -1,7 +1,13 @@
-def solution(s) :
-    numbers = [s.count(i) for i in s]
-    answer = []
-    for i in range(len(numbers)) :
-        if numbers[i] == 1 :
-            answer.append(s[i])
-    return ''.join(sorted(answer))
+#  풀이1
+# def solution(s) :
+#     numbers = [s.count(i) for i in s]
+#     answer = []
+#     for i in range(len(numbers)) :
+#         if numbers[i] == 1 :
+#             answer.append(s[i])
+#     return ''.join(sorted(answer))
+
+# 풀이2
+def solution(s) : 
+    answer = ''.join(sorted([ch for ch in s if s.count(ch) == 1]))
+    return answer
