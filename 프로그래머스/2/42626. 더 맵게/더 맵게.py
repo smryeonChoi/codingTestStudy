de856f87a5b1b551acb1264350491800e5b95291
Jiyeon <141120552+smryeonChoi@users.noreply.github.com>
2025-05-08 19:38:38 +0900

    [level 2] Title: 더 맵게, Time: 1611.31 ms, Memory: 50.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -0,0 +1,11 @@
+# heapq를 사용하게 되면 heapq[0]이 항상 가장 작은 값
+import heapq
+def solution(sco,K) :
+    heapq.heapify(sco) # 리스트 sco 자체가 힙 구조로 변환
+    ans = 0
+    while sco[0] < K :  # 힙 구조이기 때문에 항상 가장 작은 값 보장
+        # 모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에 -1
+        if len(sco) < 2 : return -1
+        heapq.heappush(sco,heapq.heappop(sco) + heapq.heappop(sco) * 2)
+        ans += 1
+    return ans
