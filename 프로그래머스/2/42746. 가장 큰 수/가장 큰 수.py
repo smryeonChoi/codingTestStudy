e3b2308d1e29e060f12d48a35c75a2c54505e4d1
Jiyeon <141120552+smryeonChoi@users.noreply.github.com>
2025-05-09 11:59:00 +0900

    [level 2] Title: 가장 큰 수, Time: 393.70 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,5 +1,19 @@
 def solution(numbers) :
+    numbers = [str(num) for num in numbers]
+    numbers = sorted(numbers,key = lambda x : x * 3, reverse=True)
+    result = ''.join(numbers)
+    return result if int(result) != 0 else '0'
+
+
+
+
+
+'''
+def solution(numbers) :
     # 세자리까지 만들수 있기 때문에
     numbers = sorted([str(i) for i in numbers],key = lambda x : str(x) * 3 ,reverse=True)
     result = ''.join(numbers)
     return result if int(result) != 0 else '0'
+'''
+
+
