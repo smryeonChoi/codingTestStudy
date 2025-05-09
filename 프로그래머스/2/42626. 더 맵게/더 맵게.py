057b74465f0ec0ff2a3f6286751c9aac197f8a0b
Jiyeon <141120552+smryeonChoi@users.noreply.github.com>
2025-05-10 00:28:59 +0900

    [level 2] Title: 더 맵게, Time: 1659.75 ms, Memory: 50.8 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -1,4 +1,16 @@
+# 리스트의 sort 사용하게 되면 매번 정렬 시행 -> 시간복잡도 증가
 # heapq를 사용하게 되면 heapq[0]이 항상 가장 작은 값
+
+import heapq
+def solution(scoville,K) :
+    heapq.heapify(scoville)
+    answer = 0
+    while scoville[0]<K:
+        if len(scoville) < 2 : return -1
+        heapq.heappush(scoville,heapq.heappop(scoville)+heapq.heappop(scoville)*2)
+        answer += 1
+    return answer
+'''
 import heapq
 def solution(sco,K) :
     heapq.heapify(sco) # 리스트 sco 자체가 힙 구조로 변환
@@ -9,3 +21,4 @@ def solution(sco,K) :
         heapq.heappush(sco,heapq.heappop(sco) + heapq.heappop(sco) * 2)
         ans += 1
     return ans
+'''
