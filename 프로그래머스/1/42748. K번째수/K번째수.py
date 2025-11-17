cf313d73f6dac8a213c6c0dad852a5fa0ba1ed10
Jiyeon <141120552+smryeonChoi@users.noreply.github.com>
2025-11-17 18:52:08 +0900

    [level 1] Title: K번째수, Time: 0.01 ms, Memory: 9.22 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.py
+++ 프로그래머스/1/42748. K번째수/K번째수.py
@@ -1,8 +1,10 @@
-def solution(array,commands) :
-    answer = []
-    for x,y,z in commands :
-        answer.append(sorted(array[x-1:y])[z-1])
-    return answer
+def solution(array,com) :
+    ans = []
+    for c in com :
+        arr = sorted(array[c[0]-1:c[1]])
+        ans.append(arr[c[2]-1])
+    return ans
+        
 
 
 
